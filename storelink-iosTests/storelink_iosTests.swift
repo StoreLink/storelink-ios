b6e1c87df099297c5553d0bfb05c12c83e0489e5
Akysh Akan <akyshakan@gmail.com>
2020-09-16 15:43:21 +0600

    Initial Commit

---
+++ storelink-iosTests/storelink_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  storelink_iosTests.swift
+//  storelink-iosTests
+//
+//  Created by Акан Акиш on 9/16/20.
+//  Copyright © 2020 Акан Акиш. All rights reserved.
+//
+
+import XCTest
+@testable import storelink_ios
+
+class storelink_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
