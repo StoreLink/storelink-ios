19c1c405a64821f29cbe4c2582b0d5774b5db60f
Akysh Akan <akyshakan@gmail.com>
2020-11-27 15:05:57 +0600

    Profile popup screen design implementation

--- storelink-ios/Sources/Profile/View/ProfilePopupViewController.swift
+++ storelink-ios/Sources/Profile/View/ProfilePopupViewController.swift
@@ -13,6 +13,9 @@ final class ProfilePopupViewController: UIViewController {
     private var hasSetPointOrigin = false
     private var pointOrigin: CGPoint?
     
+    var signupAction: (() -> Void)?
+    var loginAction: (() -> Void)?
+    
     private let topHandleView: UIView = {
         let view = UIView()
         view.backgroundColor = .lightGray
@@ -20,17 +23,54 @@ final class ProfilePopupViewController: UIViewController {
         return view
     }()
     
-    private let button: UIButton = {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Вход или регистрация"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Авторизуйтесь чтобы управлять свойм аккаунтом"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.textColor = .gray
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let loginButton: UIButton = {
         let button = UIButton()
         button.setTitle("Вход", for: .normal)
-        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        button.backgroundColor = Colors.teal.color
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
         return button
     }()
-
+    
+    private let signupButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Регистрация", for: .normal)
+        button.setTitleColor(Colors.teal.color, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        button.backgroundColor = .white
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.layer.borderColor = Colors.teal.color.cgColor
+        button.addTarget(self, action: #selector(signupButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
+        
         let swipeDownGesture = UIPanGestureRecognizer(target: self, action: #selector(swipeDownGestureAction))
         view.addGestureRecognizer(swipeDownGesture)
     }
@@ -44,16 +84,52 @@ final class ProfilePopupViewController: UIViewController {
     
     private func setupUI() {
         view.backgroundColor = .white
-        [topHandleView, button].forEach {
+        [topHandleView, titleLabel, descriptionLabel, loginButton, signupButton].forEach {
             view.addSubview($0)
         }
         
         topHandleView.snp.makeConstraints {
             $0.top.equalToSuperview().offset(15)
             $0.centerX.equalToSuperview()
-            $0.width.equalTo(45)
+            $0.width.equalTo(40)
             $0.height.equalTo(4)
         }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(topHandleView.snp.bottom).offset(15)
+            $0.left.equalToSuperview().offset(30)
+            $0.right.equalToSuperview().offset(-30)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(15)
+            $0.left.equalToSuperview().offset(30)
+            $0.right.equalToSuperview().offset(-30)
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(30)
+            $0.left.equalToSuperview().offset(30)
+            $0.right.equalToSuperview().offset(-30)
+            $0.height.equalTo(45)
+        }
+        
+        signupButton.snp.makeConstraints {
+            $0.top.equalTo(loginButton.snp.bottom).offset(15)
+            $0.left.equalToSuperview().offset(30)
+            $0.right.equalToSuperview().offset(-30)
+            $0.height.equalTo(45)
+        }
+    }
+    
+    @objc private func loginButtonTapped() {
+        self.dismiss(animated: true, completion: nil)
+        signupAction?()
+    }
+    
+    @objc private func signupButtonTapped() {
+        self.dismiss(animated: true, completion: nil)
+        loginAction?()
     }
     
     @objc private func swipeDownGestureAction(sender: UIPanGestureRecognizer) {
