65aab2672301be6e1d8c6e781f53303f6f9569a7
Akysh Akan <akyshakan@gmail.com>
2020-11-29 21:49:03 +0600

    Login screen basic ui setup

--- Storelink/Sources/Login/LoginViewController.swift
+++ Storelink/Sources/Login/LoginViewController.swift
@@ -9,10 +9,67 @@
 import UIKit
 
 class LoginViewController: InitialViewController {
+    
+    private let phoneNumberTextField: SkyFloatingLabelTextField = {
+        let textField = SkyFloatingLabelTextField()
+        textField.title = "Номер телефона"
+        textField.placeholder = "Номер телефона"
+        return textField
+    }()
+    
+    private let passwordTextField: SkyFloatingLabelTextField = {
+        let textField = SkyFloatingLabelTextField()
+        textField.title = "Пароль"
+        textField.placeholder = "Пароль"
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Вход", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        button.backgroundColor = Colors.teal.color
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Вход"
     }
     
+    override func setupUI() {
+        [phoneNumberTextField, passwordTextField, loginButton].forEach {
+            view.addSubview($0)
+        }
+        
+        phoneNumberTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(30)
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20)
+            $0.height.equalTo(50)
+        }
+        
+        passwordTextField.snp.makeConstraints {
+            $0.top.equalTo(phoneNumberTextField.snp.bottom).offset(20)
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20)
+            $0.height.equalTo(50)
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20)
+            $0.height.equalTo(45)
+        }
+    }
+    
+    @objc func loginButtonTapped() {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
